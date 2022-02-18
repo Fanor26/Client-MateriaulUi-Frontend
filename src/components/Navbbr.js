88e85aa58366f8954f5031069f7d4ed51b255a8b
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 01:08:12 -0500

    make it better

--- src/components/Navbbr.js
+++ src/components/Navbbr.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {AppBar, Toolbar, IconButton, Typography, Hidden} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import {makeStyles} from '@material-ui/core/styles';
-import ModalEntrar from './ModalEntrar'
+import ModalEntrar from './Modals/ModalEntrar'
 const useStyles= makeStyles((theme)=>({
     root:{
         flexGrow: 1
