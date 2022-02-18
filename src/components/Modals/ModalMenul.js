8f881849cba15c1e63169d84e6e6edf4420cd099
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 01:47:18 -0500

    make it better

--- src/components/Modals/ModalMenul.js
+++ src/components/Modals/ModalMenul.js
@@ -1,47 +1,9 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { List, Collapse } from "@material-ui/core";
-import { ExpandLess, ExpandMore } from "@material-ui/icons";
-
-import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingleft: theme.spacing(0),
-  },
-}));
+import React from 'react'
 
 const ModalMenul = () => {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
-      <>
-    <List
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      className={classes.root}
-    >
-      <Button onClick={handleClick}>
-        NIVEL
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </Button>
-
-      <Collapse in={open} tiemout="auto" unmountOnExit>
-        <List component="div">fanor</List>
-      </Collapse>
-    </List>
-    </>
-  );
-};
+    <div>ModalMenul</div>
+  )
+}
 
 export default ModalMenul
