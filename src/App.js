6193d78a9d631dbeb17efd2629aff700eff17725
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-17 23:05:54 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Contenedor from "./components/Contenedor";
 
 
 
@@ -10,9 +7,7 @@ import Contenedor from "./components/Contenedor";
 function App() {
   return (
     
-      <Router>
-        <Route path="/" exact component={Contenedor} />
-      </Router>
+    <div>FANOR</div>
   
   );
 }
