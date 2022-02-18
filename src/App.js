e31bcabcce2af82e47ee5ac6b465b1ae0358689d
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-17 23:10:03 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 
 
@@ -7,7 +10,9 @@ import React from "react";
 function App() {
   return (
     
-    <div>FANOR</div>
+      <Router>
+        <Route path="/" exact component={Navbar} />
+      </Router>
   
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,9 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import {AppBar, Toolbar, IconButton, Typography, Hidden} from '@material-ui/core';
+import React from 'react'
 
-import MenuIcon from '@material-ui/icons/Menu';
-
-
-
-  
-
-const useStyles= makeStyles((theme)=>({
-    root:{
-        flexGrow: 1
-    },
-    button: {
-      margin: theme.spacing(1),
-    },
-    menuButton:{
-        marginRight: '16px'
-    },
-    title:{
-        flexGrow: 1
-    },
-    imagen:{
-        borderRadius: '50%'
-    }
-    }));
-
-const Navbar =(props) => {
-    const classes= useStyles();
-    return (
-      <>
-        <div className={classes.root}>
-            <AppBar   elevation={20} color="contained">
-                <Toolbar>
-                
-                <Typography variant="h6" className={classes.title}>
-           JESÚS DE NAZARETH 
-          </Typography >
-          <Hidden smDown >
-          
-        
-       
-       
-          </Hidden>
-
-                            <Hidden only={['md', 'lg']} >
-          <IconButton variant="contained" aria-label="menu" onClick={() => props.accionAbrir()} >
-            <MenuIcon />
-            </IconButton>
-            
-                 
-          </Hidden >
-                </Toolbar>
-                
-            </AppBar>
-           
-        </div>
-        </>
-    );
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
 }
 
-export default Navbar;
+export default Navbar
