49d23781b76449a37963732a9349f2f349912d37
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-17 23:00:53 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
+import React from "react";
 
-import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-
-import Contenedor from './components/Contenedor'
-
-
-
-
-
-
-
-import { ThemeProvider } from "@material-ui/core/styles";
-import theme from "./components/ThemeConfig";
-
-
+import Contenedor from "./components/Contenedor";
 
 
 
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-    <Router>
     
-    
+      <Router>
+        <Route path="/" exact component={Contenedor} />
+      </Router>
   
-    <Route   path='/' exact component={Contenedor}/>
-
-  
-   
-    </Router>
-    </ThemeProvider>
   );
 }
 
