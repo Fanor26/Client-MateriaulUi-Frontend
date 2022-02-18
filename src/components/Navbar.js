9700da5b0274fc8d7de6f527f6589b0bf780e653
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-17 22:57:16 -0500

    make it better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import {makeStyles} from '@material-ui/core/styles';
 import {AppBar, Toolbar, IconButton, Typography, Hidden} from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/Menu';
-import BottomRF from "./dashboard/BottomRF"
+
 
 
   
