0cd488232d74de33fa249bd02b50e3011b070705
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 20:28:56 -0500

    make it better

--- src/components/primaria/primeros/primerob/Sidevarpbp.js
+++ src/components/primaria/primeros/primerob/Sidevarpbp.js
@@ -8,7 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 
-const estilos = makeStyles((theme) => ({
+const estilos = makeStyles(() => ({
   root: {
     flexGrow: 1,
   },
