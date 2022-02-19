314a8d594688540957c1a8a788943c9f1105ab9e
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-19 01:59:18 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import Axios from 'axios'
 
-Axios.defaults.baseURL='https://serverfanor.herokuapp.com/'
+Axios.defaults.baseURL='https://fanorserver.herokuapp.com/'
 
 ReactDOM.render(
   <React.StrictMode>
