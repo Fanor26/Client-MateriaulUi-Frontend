c2565b6be14b038f3f8e5ba953011ea2f755773d
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 20:11:27 -0500

    make it better

--- src/components/primaria/primeros/primeroc/Sidevarpcp.js
+++ src/components/primaria/primeros/primeroc/Sidevarpcp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {AppBar, Toolbar} from '@material-ui/core';
-import ButtonCuentaE from '../../../Modals/ButtonCuentaE'
+
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -44,7 +44,7 @@ const Sidevarpcp = (props) => {
           
         </Typography>
         
-        <ButtonCuentaE />
+        
        
       </Toolbar>
     </AppBar>
