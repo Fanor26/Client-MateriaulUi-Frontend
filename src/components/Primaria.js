f5e375867aae2f9acaa45d908fc9e81f64b1320e
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 21:09:20 -0500

    make it better

--- src/components/Primaria.js
+++ src/components/Primaria.js
@@ -2,49 +2,47 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 import "../assets/css/Dashboard.css";
 
 import CardsHeader from "./CardsHeader";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
 const BootstrapButton = withStyles({
   root: {
-    boxShadow: 'none',
-    textTransform: 'none',
+    boxShadow: "none",
+    textTransform: "none",
     fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
+    padding: "6px 12px",
+    border: "1px solid",
     lineHeight: 1.5,
-    backgroundColor: '#c68251',
-    borderColor: '#c68251',
+    backgroundColor: "#c68251",
+    borderColor: "#c68251",
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#c68251',
-      borderColor: '#c68251',
-      boxShadow: 'none',
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#c68251",
+      borderColor: "#c68251",
+      boxShadow: "none",
     },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#c68251',
-      borderColor: '#c68251',
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#c68251",
+      borderColor: "#c68251",
     },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem #c68251',
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem #c68251",
     },
   },
 })(Button);
@@ -79,8 +77,13 @@ const Primaria = () => {
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
             <CardsHeader
               icono={
-                <BootstrapButton BootstrapButton className={classes.iconos} href="#" color="primary"
-                style={{ color: '#fff'}}>
+                <BootstrapButton
+                  BootstrapButton
+                  className={classes.iconos}
+                  href="#"
+                  color="primary"
+                  style={{ color: "#fff" }}
+                >
                   "A"
                 </BootstrapButton>
               }
@@ -92,8 +95,12 @@ const Primaria = () => {
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
             <CardsHeader
               icono={
-                <BootstrapButton className={classes.iconos} href="/loginpbp" color="primary"
-                style={{ color: '#fff'}}>
+                <BootstrapButton
+                  className={classes.iconos}
+                  href="loginpbp"
+                  color="primary"
+                  style={{ color: "#fff" }}
+                >
                   "B"
                 </BootstrapButton>
               }
@@ -105,8 +112,12 @@ const Primaria = () => {
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
             <CardsHeader
               icono={
-                <BootstrapButton className={classes.iconos} href="#" color="primary"
-                style={{ color: '#fff'}}>
+                <BootstrapButton
+                  className={classes.iconos}
+                  href="#"
+                  color="primary"
+                  style={{ color: "#fff" }}
+                >
                   "C"
                 </BootstrapButton>
               }
@@ -118,11 +129,7 @@ const Primaria = () => {
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
             <CardsHeader
               titulo="SEGUNDO"
-              icono={
-                <Button className={classes.iconos} >
-                  "A"
-                </Button>
-              }
+              icono={<Button className={classes.iconos}>"A"</Button>}
               color="rgba(248,80,50,1)"
               font="white"
             />
@@ -130,11 +137,7 @@ const Primaria = () => {
           <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
             <CardsHeader
               titulo="SEGUNDO"
-              icono={
-                <Button className={classes.iconos} >
-                  "B"
-                </Button>
-              }
+              icono={<Button className={classes.iconos}>"B"</Button>}
               color="rgba(248,80,50,1)"
               font="white"
             />
@@ -262,4 +265,4 @@ const Primaria = () => {
   );
 };
 
-export default Primaria;
+export default Primaria
