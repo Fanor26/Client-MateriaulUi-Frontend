63b120a3ca5da1e12e6745950c4951726fc1b6b6
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-02-18 20:47:37 -0500

    make it better

--- src/components/ThemeConfig.js
+++ src/components/ThemeConfig.js
@@ -1,6 +1,6 @@
 import { createTheme } from '@material-ui/core/styles';
 
-import { purple, green, orange, lightBlue } from '@material-ui/core/colors';
+import {  orange } from '@material-ui/core/colors';
 
 
 const theme = createTheme({
