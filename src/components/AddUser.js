994dcdd84e6f8c8fd12663636c0296f56d24d17c
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-03-18 16:26:33 -0400

    make it better

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -17,7 +17,7 @@ const AddUser = () => {
       formData.append("image", data.image);
       formData.append("name", data.name);
 
-      const res = await fetch(`http://localhost:5000/user`, {
+      const res = await fetch(`https://juanor902.herokuapp.com/user`, {
         method: "POST",
         
         body: formData,
