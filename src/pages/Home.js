05009d2b1b4112fbd20b41f9305f9e0c909089c4
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-03-18 01:49:52 -0400

    make it better

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Viewmusaa from '../../secundaria/primeros/primeroa/Viewmusaa'
+import Viewmusaa from './././secundaria/primeros/primeroa/Viewmusaa'
 const Home = () => {
   const [users, setUsers] = useState();
 
